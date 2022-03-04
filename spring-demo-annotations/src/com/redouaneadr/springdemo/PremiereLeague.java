55576cb872365cda75d37d73fc9e6791131973e5
Redouane Adriouch <red-key@hotmail.com>
2022-03-04 13:49:25 -0500

    Adding and testing Spring Configuration with Java Code

---
+++ spring-demo-annotations/src/com/redouaneadr/springdemo/PremiereLeague.java
@@ -0,0 +1,16 @@
+package com.redouaneadr.springdemo;
+
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.redouaneadr.springdemo")
+public class PremiereLeague {
+	
+	//private Team team;
+	
+
+}
