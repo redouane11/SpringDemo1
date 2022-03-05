4f879d2a6d98e7ffed599ab1230b924d4d05d9de
Redouane Adriouch <red-key@hotmail.com>
2022-03-04 23:50:38 -0500

    Config and set up a  Spring MVC project

---
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-page.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spring-mvc-demo</title>
+</head>
+<body>
+	<h1>Hello from Spring MVC</h1>
+</body>
+</html>
---
+++ spring-mvc-demo/src/com/redouaneadr/springdemo/mvc/HomeController.java
@@ -0,0 +1,15 @@
+package com.redouaneadr.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showHomePage() {
+		
+		return "main-page";
+	}
+
+}
