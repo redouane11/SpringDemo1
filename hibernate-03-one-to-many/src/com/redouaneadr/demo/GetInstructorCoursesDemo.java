1f3012902157db71023830b27bad918fec16c16e
Redouane Adriouch <red-key@hotmail.com>
2022-04-04 19:04:56 -0400

    Hibernate Mapping: OneToMany, implementating and testing

---
+++ hibernate-03-one-to-many/src/com/redouaneadr/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,55 @@
+package com.redouaneadr.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.redouaneadr.demo.entity.Course;
+import com.redouaneadr.demo.entity.Instructor;
+import com.redouaneadr.demo.entity.InstructorDetail;
+
+
+public class GetInstructorCoursesDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {	
+			
+			
+			// start a transaction
+			session.beginTransaction();
+
+			// get the instructor from db
+			int theId = 1;
+			Instructor tempInstr = session.get(Instructor.class, theId);
+			
+			// get course from the instructor some courses
+			
+			System.out.println("Fetching from db ..." );
+			System.out.println("Course: " + tempInstr.getCourses());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// asd clean up code
+			session.close();
+			factory.close();
+		}
+	}
+
+}
